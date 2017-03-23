b6b40d27f641467ad69e369949df7e592191c22b
0x90 <abdi.moya@gmail.com>
2017-03-23 13:32:24 -0400

    Updates 3.23.2017
    Small updates

---
+++ dnSpy-StringsAnalyzer/StringAnalyzer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using dnSpy.Contracts.Extension;
+
+// Each extension should export one class implementing IExtension
+
+namespace Plugin.StringAnalyzer {
+	[ExportExtension]
+	internal sealed class StringAnalyzer : IExtension {
+		public IEnumerable<string> MergedResourceDictionaries {
+			get {
+				yield return "Themes/resourcedict.xaml";
+			}
+		}
+
+		public ExtensionInfo ExtensionInfo { get; } = new ExtensionInfo
+		{
+		    ShortDescription =
+		        $"This plugin will read the whole assembly and gives you a detailed list view with all the found strings inside of a given assembly."
+		};
+
+	    public void OnEvent(ExtensionEvent @event, object obj) {
+			// We don't care about any events
+		}
+	}
+}
